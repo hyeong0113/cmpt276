f89b724fc9c62d0f93f799779dc1b908bde5eab6
Hyung June Kwak <kwakdent95@gmail.com>
2021-05-19 16:07:15 -0700

    Weight grade function created_2105191602

--- assignment1/calculator.js
+++ assignment1/calculator.js
@@ -6,20 +6,15 @@ function meanGrade() {
 
     var i;
     var count = 0;
-    var myGradeArray = [];
-    var baseGradeArray = [];
 
     for (i = 0; i < myGradeElement.length; i++) {
-        if ((myGradeElement[i].value !== "" && baseGradeElement[i].value === "") &&
-            (myGradeElement[i].value === "" && baseGradeElement[i].value !== "")) {
-            console.log("You need to insert both grades in all activities!");
+        if ((!myGradeElement[i].value && baseGradeElement[i].value) ||
+            (myGradeElement[i].value && !baseGradeElement[i].value)) {
+            console.error("You need to insert both grades in all activities!");
             continue;
         }
 
-        if (myGradeElement[i].value !== "" && baseGradeElement[i].value !== "") {
-            console.log(myGradeElement[i].value);
-            console.log(baseGradeElement[i].value);
-
+        if (myGradeElement[i].value && baseGradeElement[i].value) {
             totalGrade += (parseInt(myGradeElement[i].value)) / (parseInt(baseGradeElement[i].value));
             count++;
         }
@@ -27,4 +22,33 @@ function meanGrade() {
     }
 
     document.getElementById("result-meanGrade").innerHTML = (totalGrade / count);
+}
+
+function weightGrade() {
+    var myGradeElement = document.getElementsByName("my-grade");
+    var baseGradeElement = document.getElementsByName("base-grade");
+    var weightGradeElement = document.getElementsByName("weight");
+
+    var weightedTotalGrade = 0;
+    var weightTotal = 0;
+    var i;
+
+    for (i = 0; i < myGradeElement.length; i++) {
+        if ((!myGradeElement[i].value && baseGradeElement[i].value) ||
+            (myGradeElement[i].value && !baseGradeElement[i].value)) {
+            console.error("You need to insert both grades in all activities!");
+            continue;
+        }
+
+        if (myGradeElement[i].value && baseGradeElement[i].value) {
+            if (weightGradeElement[i]) {
+                weightTotal += parseInt(weightGradeElement[i].value);
+                weightedTotalGrade += ((parseInt(myGradeElement[i].value)) / (parseInt(baseGradeElement[i].value)) * weightGradeElement[i].value);
+            } else {
+                console.error("You need to insert weight value for calculation!");
+            }
+        }
+    }
+
+    document.getElementById("result-meanGrade").innerHTML = (weightedTotalGrade / weightTotal);
 }
